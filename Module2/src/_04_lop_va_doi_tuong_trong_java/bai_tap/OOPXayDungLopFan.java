a66997afee02d039e2bff6a33f92fc370d88565f
trung <trunghoa130299@gmail.com>
2021-01-20 20:35:10 +0700

    20/1

--- Module2/src/_04_lop_va_doi_tuong_trong_java/bai_tap/OOPXayDungLopFan.java
+++ Module2/src/_04_lop_va_doi_tuong_trong_java/bai_tap/OOPXayDungLopFan.java
@@ -2,10 +2,12 @@ package _04_lop_va_doi_tuong_trong_java.bai_tap;
 
 public class OOPXayDungLopFan {
     public static void main(String[] args) {
-        final int slow =1,medium=2,fast=3;
+        final int SLOW =1;
+        final int MEDIUM=2;
+        final int FAST=3;
         double radius;
-        XayDungLopFan Fan1= new XayDungLopFan(fast,true,10,"yellow");
-        XayDungLopFan Fan2= new XayDungLopFan(medium,false,5,"blue");
+        XayDungLopFan Fan1= new XayDungLopFan(FAST,true,10,"yellow");
+        XayDungLopFan Fan2= new XayDungLopFan(MEDIUM,false,5,"blue");
         System.out.println(Fan1.toString());
         System.out.println(Fan2.toString());
     }
--- Module2/src/_04_lop_va_doi_tuong_trong_java/bai_tap/XayDungLopFan.java
+++ Module2/src/_04_lop_va_doi_tuong_trong_java/bai_tap/XayDungLopFan.java
@@ -1,8 +1,10 @@
 package _04_lop_va_doi_tuong_trong_java.bai_tap;
 
 public class XayDungLopFan {
-    final int slow =1,medium=2,fast=3;
-    private int speed = slow;
+    final int SLOW =1;
+    final int MEDIUM=2;
+    final int FAST=3;
+    private int speed = SLOW;
     private boolean on = false;
     private double radius =5;
     private String color = "blue";
@@ -15,15 +17,15 @@ public class XayDungLopFan {
     }
 
     public int getSlow() {
-        return slow;
+        return SLOW;
     }
 
     public int getMedium() {
-        return medium;
+        return MEDIUM;
     }
 
     public int getFast() {
-        return fast;
+        return FAST;
     }
 
     public int getSpeed() {
