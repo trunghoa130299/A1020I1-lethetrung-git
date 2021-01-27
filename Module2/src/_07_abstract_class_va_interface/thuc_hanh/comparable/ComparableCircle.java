3993477aa18bd6d80d6ede2bf6f542eb732ab950
trung <trunghoa130299@gmail.com>
2021-01-27 20:17:13 +0700

    27/1

---
+++ Module2/src/_07_abstract_class_va_interface/thuc_hanh/comparable/ComparableCircle.java
@@ -0,0 +1,37 @@
+package _07_abstract_class_va_interface.thuc_hanh.comparable;
+
+import _06_ke_thua.thuc_hanh.he_cac_doi_tuong_hinh_hoc.Circle;
+
+import java.util.Arrays;
+
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle> {
+    public ComparableCircle() {
+    }
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5, "indigo", false);
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+        Arrays.sort(circles);
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
---
+++ Module2/src/_07_abstract_class_va_interface/thuc_hanh/comparator/CircleComparator.java
@@ -0,0 +1,14 @@
+package _07_abstract_class_va_interface.thuc_hanh.comparator;
+
+import _06_ke_thua.thuc_hanh.he_cac_doi_tuong_hinh_hoc.Circle;
+
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
+    }
+}
