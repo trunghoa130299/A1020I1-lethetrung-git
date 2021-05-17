0fb9ab668af7ab31372234dbd0d1b64e7c5382d0
trung <trunghoa130299@gmail.com>
2021-05-17 21:37:10 +0700

    17/5

--- Module3_WBD/12_jdbc_crud/bai_tap/user_management.sql
+++ Module3_WBD/12_jdbc_crud/bai_tap/user_management.sql
@@ -15,3 +15,61 @@ create table users (
 insert into `users` values(1,'Nguyễn Văn A','nguyenvana@codegym.vn','Việt Nam'),
 (2,'Nguyễn Văn B','nguyenvanb@codegym.vn','Việt Nam'),
 (3,'Nguyễn Văn C','nguyenvanc@codegym.vn','Việt Nam');
+
+DELIMITER $$
+
+CREATE PROCEDURE get_user_by_id(IN user_id INT)
+
+BEGIN
+
+    SELECT users.`name`, users.email, users.country
+
+    FROM users
+
+    where users.id = user_id;
+
+    END$$
+
+DELIMITER ;
+call get_user_by_id(1);
+
+DELIMITER $$
+CREATE PROCEDURE delete_user_by_id(IN user_id INT)
+
+BEGIN
+
+    delete 
+    FROM users
+    where users.id = user_id;
+    END$$
+DELIMITER ;
+
+call delete_user_by_id(3);
+DELIMITER $$
+CREATE PROCEDURE update_user_by_id(IN user_name varchar(50),
+
+IN user_email varchar(50),
+
+IN user_country varchar(50),
+IN user_id INT)
+
+BEGIN
+
+    update users
+    set `name` = user_name , email= user_email,country =user_country
+    where users.id = user_id;
+    END$$
+DELIMITER ;
+
+call update_user_by_id('trung','abc@abc.abc','nghe an',3);
+
+DELIMITER $$
+CREATE PROCEDURE list_user()
+
+BEGIN
+    select * 
+	from users;
+    END$$
+DELIMITER ;
+call list_user;
+
